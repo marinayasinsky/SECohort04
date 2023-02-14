7ef9c6a4b9ca68c760b5409588bbaf0d8819310a
marinayasinsky <122933750+marinayasinsky@users.noreply.github.com>
2023-02-14 16:00:35 -0500

    last commit

--- Module_1/total_recal/index.js
+++ Module_1/total_recal/index.js
@@ -10,42 +10,135 @@
 
 
 // I. Variables & Data Types
-// A. Q + A
+// A. Q +  A
 // B. Strings
+// let firstVariable = "Hello World";
+// firstVariable = 5;
+// let secondVariable = firstVariable;
+// let yourName = "Hello, my name is Marina";
+
+// console.log(firstVariable);
+// console.log(secondVariable);
+// console.log(yourName);
+
 // C. Booleans
+// const a = 4;
+// const b = 53;
+// const c = 57;
+// const d = 16;
+// const e = 'Kevin';
+
+// console.log(a < b);
+// console.log(c > d);
+// console.log('Name' === 'Name');
+// // FOR THE NEXT TWO, USE ONLY && OR ||
+// console.log(true || false);
+// console.log(false && false && false && false && false || true);
+// console.log(false === false)
+// console.log(e === 'Kevin');
+// console.log(a != b != c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+// console.log(a === a || d); // note: the answer is a simple arithmetic equation, not something "weird"
+// console.log(48 >= '48');
+
+
 // D. The Farm
-// E. Driver's Ed
-//_____________________________________________________________
+// let animal = "";
+// if (animal === "cow"){
+// 	console.log("mooooo");
+// } else {
+// 	console.log("Hey! You're not a cow.");
+// }
+// animal = "cow";
 
 
-
+// // E. Driver's Ed
+// let personAge = 18;
+// if (personAge >= 16){
+// 	console.log("Here are the keys.");
+// } else {
+// 	console.log("Sorry, you're too young.");
+// }
+// personAge = 21;
 
 
 
 // II. Loops
 // A. The Basics
+// for (let a = 0; a < 11; a++){
+// 	console.log(a);
+// };
+// for (a = 10; a < 4001; a++){
+// 	console.log(a);
+// };
+// for (a = 10; a < 4001; a += 2){
+// 	console.log(a);
+// };
 // B. Get even
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//       console.log(i + ' <-- is an even number');
+//     } else {
+//       console.log(i);
+//     }
+// }
+
+
+
 // C. Give me Five
+// for (let i = 0; i < 100; i++) {
+// if ( 0 === i % 5 && 3 === 0 ) {
+//     console.log("I found a " + i + ". High five!");
+// 	console.log("I found a " + i + ". Three is a crowd");
+// } else if ( i % 5 === 0) {
+//     console.log("I found a " + i + ". High five!");
+// } else if (i % 3 === 0) {
+//     console.log("I found a " + i + ". Three is a crowd" );
+// }
+// }
+
 // D. Savings account
-//_____________________________________________________________
+// let bank_account = 0
+// for (let i = 0; i < 11; i++) {
+//     bank_account +=i;
+// }
+// console.log(bank_account);
 
-
-
-
-
-
-
+// bank_account = 0
+// for (let i =0; i <101; i++ ) {
+//     bank_account += i * 2;
+// }
+// console.log(bank_account);
 
 // III. Arrays & Control Flow
 // A. Talk about it:
 // B. Easy Does It
+let quotes = ["Let it be!", "Fun times!", "Let roll!"];
+console.log(quotes);
+
 // C. Accessing elements
+const randomThings = [1, 10, "Hello", true];
+randomThings [0];
+randomThings [2] = "World";
+console.log(randomThings);
+
 // D. Change values
+const ourClass =["Salty", "Zoom", "Sardine", "Slack", "Github"];
+ourClass[2];
+ourClass[4] = "Octocat";
+ourClass.push ("Cloud City");
+
+console.log(ourClass);
+
 // E. Mix It Up
+const myArray = [5, 10, 500, 20]
+myArray.push = "Aegon";
+myArray.unshift 
+
+console.log(myArray);
+
 // F. Biggie Smalls
 // G. Monkey in the Middle
 // H. What's in Your Closet?
-//_____________________________________________________________
 
 
 
