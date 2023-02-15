e23c9dfa46de4552ef50c16744715a71362564a8
marinayasinsky <122933750+marinayasinsky@users.noreply.github.com>
2023-02-15 15:58:22 -0500

    day 2 total recall

--- Module_1/total_recal/d2_total_recal.js
+++ Module_1/total_recal/d2_total_recal.js
@@ -1,39 +1,140 @@
 ////////////////////////////////
 //* Easy Going
-////////////////////////////////
+//Write a for loop that will log the numbers 1 through 20.
+// for ( let i = 0 ; i < 20 ; i++) {
+//     console.log("Easy Going answered");
+// }
 
 
-////////////////////////////////
 //* Get Even
-////////////////////////////////
+//Write a for loop that will log only the even numbers in 0 through 200.
+// for (let i = 0; i <100; i+=2) {
+// console.log(i);
+// console.log("Get Even answered");
+// }
 
-
-////////////////////////////////
 //* Fizz Buzz
-////////////////////////////////
+//This is a classic problem that you should get really comfortable solving. If you've solved it before, try to make it more elegant and short.
+
+// Write a javascript application that logs all numbers from 1 - 100.
+// If a number is divisible by 3 log "Fizz" instead of the number.
+// If a number is divisible by 5 log "Buzz" instead of the number.
+// If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
 
 
+// for (let i =0; i<=100; i++){
+//     if ( i % 3 ==0 && i % 5 == 0) {
+//         console.log("FizzBuzz");
+//     } else if ( i % 3 === 0) {
+//         console.log("Fizz");
+//     } else if ( i % 5 === 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log("FizzBuzz");
+//     }
+// }
 ////////////////////////////////
 //* Wild Wild Life
-////////////////////////////////
+// Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
+// Use a for of loop(not a typo - try it out! Try a for ofloop) to call toUpperCase()on each of them and print out the result.
+// As a developer, you'll be a lifelong learner and constantly encountering new things. We'll give you little stretches like this to get you used to looking at documentation and figuring some things out on your own. You've got this!
+
+
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+// porgee[2] +=1;
+// console.log(porgee);
+//  wolfy[3] = "Gotham City";
+//  console.log(wolfy);
+//  dart.push("Hawkins");
+//  console.log(dart);
+//  wolfy.splice(0,1,"Gameboy");
+// console.log(wolfy);
 
 
 ////////////////////////////////
 //* Yell at the Ninja Turtles
-////////////////////////////////
+//Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
+//Use a for of loop(not a typo - try it out! Try a for ofloop) to call toUpperCase()on each of them and print out the result.
+// As a developer, you'll be a lifelong learner and constantly encountering new things. We'll give you little stretches like this to get you used to looking at documentation and figuring some things out on your own. You've got this!
 
+// let ninjaTurtles = [ 'Donatello', 'Leonardo', 'Raphael', 'Michaelangelo']
+// console.log(ninjaTurtles);
+// for (const element of ninjaTurtles) {
+//     console.log (element.toUpperCase());
+//      }
+   
 
-////////////////////////////////
 //* Methods, Revisited
-////////////////////////////////
 
+// const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
 
-////////////////////////////////
+// //Console log: the index of Titanic
+// console.log (favMovies.indexOf("Titanic"));
+
+// // Use the method pop
+// favMovies.pop();
+
+// // push"Guardians of the Galaxy"
+// favMovies.push("Guardians of the Galaxy");
+
+// //Reverse the array
+// favMovies.reverse();
+
+// //Use the shift method
+// favMovies.shift();
+
+// // unshift- what does it return?
+// console.log(favMovies.unshift('Snatch'));
+// console.log(favMovies);
+
+// // splice"Django Unchained" and add "Avatar" (try finding the index of "Django Unchained", instead of counting it yourself) Thought question: did this permanently alter our array?
+
+// let index = favMovies.indexOf("Django Unchained");
+// favMovies.splice(index, 1, 'Avatar')
+
+// // slicethe last half of the array (challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it) - Thought question: did this permanently alter our array?
+ 
+// mid = Math.ceil(favMovies.length/2),
+// obj = {
+//     right: favMovies.slice(mid)
+// }
+
+// // store the value of your slicein a variable, console.log it - Thought question: what is going on here?
+// console.log(favMovies.slice(mid));
+
+// // console.log your final results
+// console.log (favMovies);
+
+// // After running the above tasks, console.log the index of "Fast and Furious" -We removed it from the array, what value do we get when we look for the index of something that is not in the array?
+
+// console.log(favMovies.indexOf("Fast and Furious"))
+
+// // Thought question: that we declared the variable favMovieswith const, and yet, we were allowed to change the array. Weird? Should we have used let?
+
+// console.log("Methods Revisited answered")
+
 //* Where is Waldo
-////////////////////////////////
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", ["Baked Goods", "Waldo"]]];
 
 
-////////////////////////////////
+
+//Remove Eggbert (hint look at the slice/splice method(s))
+whereIsWaldo.slice(whereIsWaldo.indexOf("Eggbert"),1); 
+// Change "Neff" to "No One"
+// Access and console.log "Waldo"
+
+console.log (whereIsWaldo[0]);//[ 'Timmy', 'Frank' ]
+console.log (whereIsWaldo[1]);//Eggbert
+console.log (whereIsWaldo[2]);//[ 'Lucinda', 'Jacc', 'Neff', 'Snoop' ]
+console.log (whereIsWaldo[3]);//[ 'Petunia', [ 'Baked Goods', 'Waldo' ] ]
+
 //*  Excited Kitten
 ////////////////////////////////
 
--- Module_1/total_recal/index.js
+++ Module_1/total_recal/index.js
@@ -11,7 +11,15 @@
 
 // // I. Variables & Data Types
 // // A. Q +  A
-// // B. Strings
+// // B. Strings For all other questions that involve writing code, you can solve them via the following instructions.
+// Create a variable called firstVariable
+// Assign it the value of the string "Hello World"
+// Change the value of this variable to some number
+// Store the value of firstVariablein a new variable called secondVariable
+// Change the value of secondVariableto any string.
+// What is the value of firstVariable?
+// Create a variable called yourNameand set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourNameso that it returns a new string with them concatenated.
+
 // // let firstVariable = "Hello World";
 // // firstVariable = 5;
 // // let secondVariable = firstVariable;
@@ -22,6 +30,7 @@
 // // console.log(yourName);
 
 // // C. Booleans
+// Using the provided variable definitions, replace the blanks so that all log statements print truein the console. Answers should be all be valid JS syntax and not weird things that don't make sense but happen to print trueto the console
 // // const a = 4;
 // // const b = 53;
 // // const c = 57;
@@ -42,6 +51,10 @@
 
 
 // // D. The Farm
+// Write code that will print out "mooooo" if the it is equal to cow
+// Change your code so that if the variable animalis anything other than a cow, it will print "Hey! You're not a cow."
+// Commit
+
 // // let animal = "";
 // // if (animal === "cow"){
 // // 	console.log("mooooo");
@@ -52,6 +65,9 @@
 
 
 // // // E. Driver's Ed
+// Make a variable that holds a person's age; be semantic
+// Write code that will print out "Here are the keys!", if the age is 16 years or older, or, if the age is younger than 16, a message should print "Sorry, you're too young."
+
 // // let personAge = 18;
 // // if (personAge >= 16){
 // // 	console.log("Here are the keys.");
@@ -62,8 +78,15 @@
 
 
 
-// // II. Loops
+// // II. LoopsRemember: USE letwhen you initialize your for loops!
+// This is GOOD: for(let i = 0; i < 100; i++)
+// This is NO GOOD: for(i = 0; i < 100; i++)
+
 // // A. The Basics
+// Write a loop that will print out all the numbers from 0 to 10, inclusive
+// Write a loop that will print out all the numbers from 10 up to and including 400
+// Write a loop that will print out every third number starting with 12 and going no higher than 4000
+
 // // for (let a = 0; a < 11; a++){
 // // 	console.log(a);
 // // };
@@ -74,6 +97,9 @@
 // // 	console.log(a);
 // // };
 // // B. Get even
+// Print out the numbers that are within the range of 1 - 100
+// Adjust your code to add a message next to even numbers only that says: "<-- is an even number"
+
 // // for (let i = 1; i <= 100; i++) {
 // //     if (i % 2 === 0) {
 // //       console.log(i + ' <-- is an even number');
@@ -85,10 +111,16 @@
 
 
 // // C. Give me Five
+// For the numbers 0 - 100, print out "I found a number. High five!" if the number is a multiple of five
+// Add to the code from above to print out "I found a number. Three is a crowd" if the number is a multiple of three
+
 // // for (let i = 0; i < 100; i++) {
 // // if ( 0 === i % 5 && 3 === 0 ) {
 // //     console.log("I found a " + i + ". High five!");
 // // 	console.log("I found a " + i + ". Three is a crowd");
+
+//For numbers divisible by both three and five, be sure your code prints both messages
+
 // // } else if ( i % 5 === 0) {
 // //     console.log("I found a " + i + ". High five!");
 // // } else if (i % 3 === 0) {
@@ -96,7 +128,11 @@
 // // }
 // // }
 
-// // D. Savings account
+// // D. Savings account Write code that will save the sum of all the numbers between 1 - 10 to a variable called bank_account.
+// Check your work! Your bank_account should have $55 in it.
+// You got a bonus! Your pay is now doubled each week. Write code that will save the sum of all the numbers between 1 - 100 multiplied by 2.
+// Check your work! Your bank_account should have $10,100 in it.
+
 // let bank_account = 0
 // for (let i = 0; i < 11; i++) {
 //     bank_account +=i;
@@ -111,7 +147,9 @@
 
 // // III. Arrays & Control Flow
 // // A. Talk about it:
-// // B. Easy Does It
+// // B. Easy Does It B. Easy Does It
+// Create an array that contains three quotes and store it in a variable called quotes
+
 // let quotes = ["Let it be!", "Fun times!", "Let roll!"];
 // console.log(quotes);
 
@@ -265,34 +303,112 @@
 // // H. printLongestWord Write a function printLongestWordthat accepts a single argument, an array of strings. The method should return the longest word in the array. In case of a tie, the method should return the word that appears first in the array.
 
 function printLongestWord(singleArg){
-    var longestWord = singleArg[0].length;
-    var longestStrin = singleArg[0];
-
-    for (i = 0; i < singleArg.length; i ++) {
- if (singleArg[i].length > longestWord) {
-    longestStrin = singleArg[i];
-    longestWord = singleArg[i].length;
+    let longestWord = "";
+    for (let i = 0; i < singleArg.length; i ++) {
+ if (singleArg[i].length > longestWord.length) {
+    longestWord = singleArg[i];
 }
     } 
     return longestWord;
 
 }
 console.log(printLongestWord(["BoJack", "Princess", "Diane", "a", "Max", "Peanutbutter", "big", "Todd"]));
-// //_____________________________________________________________
+// // //_____________________________________________________________
 
 
+// //* V. Objects
+// // A.Make a user object Create an object called user. Write in to the object the key-value pairs for name, email, age, and purchased. Set the value of purchasedto an empty array []. Set the other values to whatever you would like.
+
+const user = { 
+    name: "Billy" ,
+    email: "billy@gmail.com",
+    age: "15",
+    purchased: [],
+    friend: {
+        name: "",
+        age: "85",
+        location: "",
+        purchased: [],
+    }
+};
+
+// // B. Update the user
+// Our user has changed his or her email address. Without changing the original userobject, update the emailvalue to a new email address.
+// Our user has had a birthday! Without changing the original userobject, increment the agevalue using the postfix operator. Hint: age++
+
+user.email = "billy@mail.com";
+console.log(user.email);
+
+user.age++
+console.log(user.age)
+// // C.Adding keys and values
+// You have decided to add your user's location to the data that you want to collect.
+
+// Without changing the original userobject, add a new key locationto the object, and give it a value or some-or-other location (a string).
+
+user ["location"] = "New York";
+console.log(user.location);
+// // D.Shopaholic!
+// Our user has purchased an item! They have purchased some "carbohydrates". Using .push(), add the string "carbohydrates" to the purchasedarray.
+// Our user has purchased an item! They have purchased some "peace of mind". Using .push(), add the string "peace of mind" to the purchasedarray.
+// Our user has purchased an item! They have purchased some "Merino jodhpurs". Using .push(), add the string "Merino jodhpurs" to the purchasedarray.
+// Console.log just the "Merino jodhpurs" from the purchasedarray.
+
+user.purchased.push("carbohydrates", "peace of mind","Merino jodhpurs" )
+console.log(user.purchased[2]);
+// //* E. Object-within-object
+// Write a friendobject into your userobject and give the friend a name, age, location, and purchased array (empty for now)
+// Console.log just the friend's name
+// Console.log just the friend's location
+// CHANGE the friend's age to 55
+// The friendhas purchased "The One Ring". Use .push()to add "The One Ring" to the friend's purchasedarray.
+// The friendhas purchased "A latte". Use .push()to add "A latte" to the friend's purchasedarray.
+// Console.log just "A latte" from the friend's purchasedarray.
+
+user.friend.name = "Sam"; 
+console.log(user.friend.name);
+user.friend.location = "Washington";
+console.log(user.friend.location);
+user.friend ["age"] = 55;
+console.log(user.friend.age);
+user.friend.purchased.push("The One Ring", "A latte" );
+console.log(user.friend.purchased[1]);
+// //* F.Loops
+// Write a for loop that iterates over the User's purchasedarray (NOT the friend's purchased array), and prints each element to the console.
+// Write a for loop that iterates over the Friend's purchasedarray, and prints each element to the console.
+
+for ( let i =0; i < user.purchased.length; i++ ) {
+ console.log(user.purchased[i]);
+}
+
+for ( let c = 0; c < user.friend.purchased.length; c++) {
+    console.log(user.friend.purchased[c]);
+}
+
+// //* G.Functions can operate on objects
+// Write a single function updateUserthat takes no parameters. When the function is run, it should:
+// it should increment the user's age by 1
+// make the user's name uppercase
+// The function does not need a returnstatement, it will merely modify the user object.
+
+// Write a function oldAndLoudthat performs the exact same tasks as updateUser, but instead of hard-coding it to only work on our userobject, make it take a parameter person, and have it modify the object that is passed in as an argument when the function is called. Call your oldAndLoudfunction with useras the argument.
 
 
+// function updateUser()  {
+//     user.age++;
+//     user.name = user.name.toUpperCase();
+// }
+// updateUser();
+// console.log(user);
 
+function oldAndLoud (person) {
+    person.age++;
+    person.name = person.name.toUpperCase();
 
-// // V. Objects
-// // A.
-// // B.
-// // C.
-// // D.
-// // E.
-// // F.
-// // G.
+}
+oldAndLoud(user);
+console.log(user);
+
 // //_____________________________________________________________
 
 
