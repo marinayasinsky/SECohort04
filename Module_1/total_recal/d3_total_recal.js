2650d41cdb126660b708e76a275439552c86e14c
marinayasinsky <122933750+marinayasinsky@users.noreply.github.com>
2023-02-16 15:55:50 -0500

    day 3 total recall

--- Module_1/total_recal/d3_total_recal.js
+++ Module_1/total_recal/d3_total_recal.js
@@ -14,3 +14,55 @@ class Hamster {
       return this.price
     }
   }
+
+console.log("created hamster class");
+
+class Person {
+    constructor(name){
+        this.name = name 
+        this.age = 0
+        this.height = 0
+        this.weight = 0
+        this.mood = 0
+        this.hamster = " "
+        this.bankAccount = 0
+    }
+    getName(){
+        return this.name 
+    }
+
+    getAge() {
+        return this.age
+    }
+    getWeight() {
+        return this.age
+    }
+    greet(){
+        console.log(` I am ${this.name} hello!`);
+    }
+    eat (){
+        this.weight++
+        this.mood++
+    }
+    exercise() {
+        this.weight--
+    }
+    ageUp() {
+        this.age++
+        this.height++
+        this.weight++
+        this.mood--
+        this.bankAccount+=10
+    }
+    buyHamster(hamster){
+        this.hamster.push(hamster)
+        this.mood+=10
+        this.bankAccount-=hamster.getPrice()
+
+    }
+}
+console.log("created person class");
+
+class newPerson {
+    constructor
+}
